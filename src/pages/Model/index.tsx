d64727f53ea363aa4d72479989a2a3d5f0f32182
Ruan Belmiro <ruan.carlos.belmiro@gmail.com>
2025-10-27 18:18:24 -0300

    add criacao do inputText nao finalizado

--- src/pages/Model/index.tsx
+++ src/pages/Model/index.tsx
@@ -1,9 +1,15 @@
 import { View, Text } from 'react-native';
+import { Input } from '../../ui/Input';
 
 export function Model() {
   return (
-    <View>
-      <Text>Model Page</Text>
+    <View style={{ alignItems: 'center' }}>
+      <Text style={{ margin: 10 }}>Model Page</Text>
+
+      <View style={{ width: 300 }}>
+        <Input placeholder="Digite aqui" />
+      </View>
+      <Text style={{ alignItems: 'center', marginTop: 10 }}>Input Content</Text>
     </View>
   );
 }
--- src/ui/Input/index.tsx
+++ src/ui/Input/index.tsx
@@ -1,6 +1,48 @@
-import { View } from 'react-native';
-import { container } from './styles';
+import { Text, View } from 'react-native';
+import { container, content, inputKitStyle } from './styles';
+import { Input as InputKit } from '@ui-kitten/components';
+import { EvaSize, EvaStatus } from '@ui-kitten/components/devsupport';
 
-export function Input() {
-  return <View style={container} />;
+interface IInputProps {
+  disabled?: boolean;
+  value?: string;
+  size?: EvaSize;
+  placeholder?: string;
+  label?: string;
+  onChange?: () => void;
+  iconLeft?: React.ReactElement;
+  iconRight?: React.ReactElement;
+  status?: EvaStatus;
+}
+
+export function Input({
+  disabled,
+  value,
+  size = 'medium',
+  placeholder,
+  label,
+  onChange,
+  iconLeft,
+  iconRight,
+  status = 'primary',
+}: IInputProps) {
+  return (
+    <View style={container}>
+      <View style={content}>
+        <InputKit
+          disabled={disabled}
+          value={value}
+          size={size}
+          placeholder={placeholder}
+          label={label}
+          secureTextEntry={true}
+          onChangeText={onChange}
+          accessoryLeft={iconLeft}
+          accessoryRight={iconRight}
+          status={status}
+          style={inputKitStyle}
+        />
+      </View>
+    </View>
+  );
 }
