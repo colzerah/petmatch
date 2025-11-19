ac803a5e6c0d8dbc94e5e69ca17fde25df6f8e0d
Ruan Belmiro <ruan.carlos.belmiro@gmail.com>
2025-11-19 18:14:32 -0300

    add componente Menu

---
+++ src/components/Menu/menuDTO.ts
@@ -0,0 +1,30 @@
+import { IconName } from '@/dtos/iconDTO';
+
+const MENU_PLACEMENTS = [
+  'top',
+  'bottom',
+  'left',
+  'right',
+  'top left',
+  'top right',
+  'bottom left',
+  'bottom right',
+  'right top',
+  'right bottom',
+  'left top',
+  'left bottom',
+] as const;
+export type MenuPlacement = (typeof MENU_PLACEMENTS)[number];
+
+export interface MenuItem {
+  value: string;
+  iconName?: IconName;
+}
+
+export interface MenuProps {
+  items: MenuItem[];
+  placement?: MenuPlacement;
+  offset?: number;
+  disabledKeys?: string[];
+  title?: string;
+}
