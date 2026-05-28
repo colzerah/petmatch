6e8844bcc6f67e0fba31fc215f0c413ad47dad79
Ruan Belmiro <ruan.carlos.belmiro@gmail.com>
2026-05-28 20:04:03 -0300

    feat: create componente RadioCard

--- src/pages/Auth/RegisterScreens/RegisterType/index.tsx
+++ src/pages/Auth/RegisterScreens/RegisterType/index.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+
 import { Box } from '@/components/ui/box';
 import { VStack } from '@/components/ui/vstack';
 
 import { Typography } from '@/components/Typography';
+import { RadioCard } from '@/components/RadioCard';
 
 export function RegisterType() {
+  const [selected, setSelected] = useState('user');
+
   return (
     <Box className="flex flex-1 p-8 bg-primary-50">
       <VStack space="lg">
@@ -12,7 +17,27 @@ export function RegisterType() {
         </Box>
 
         <VStack>
-          <Typography title="Em breve!" />
+          <RadioCard
+            options={[
+              {
+                title: 'Usuário',
+                subTitle: 'Cadastro para tutor e uso pessoal do App',
+                value: 'user',
+              },
+              {
+                title: 'Estabelecimento Comercial',
+                subTitle: 'Pet Shop, Clínica, loja ou empresa parceira.',
+                value: 'establishment',
+              },
+              {
+                title: 'Serviço Autônomo',
+                subTitle: 'Profissionais independentes que atendem pets',
+                value: 'service',
+              },
+            ]}
+            value={selected}
+            onChange={setSelected}
+          />
         </VStack>
       </VStack>
     </Box>
